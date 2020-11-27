97a410f32a3bd24638c7fe3cf4890066cec41a67
Felipe Flores <49493965+felipefloresfaf@users.noreply.github.com>
2020-11-27 17:20:54 -0300

    Add files via upload

---
+++ SistemaCaixaMercado/build/classes/frames/CompraFrame.rs
@@ -0,0 +1,4 @@
+frames.CompraFrame
+frames.CompraFrame$2
+frames.CompraFrame$1
+frames.Callback
---
+++ SistemaCaixaMercado/build/classes/frames/MainFrame.rs
@@ -0,0 +1,5 @@
+frames.Theme
+frames.MainFrame
+frames.MainFrame$1
+frames.MainFrame$3
+frames.MainFrame$2
